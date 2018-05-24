cb995a212199864d762d2fa1cd9e2a17efd7a810
Edsel Caprice <caprice@mail.usf.edu>
2018-05-24 00:44:50 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,18 @@ const codes = [
   "a"
 ];
 
+
+index = 0
+body = document.getElementByID('body')
 function init() {
-  // your code here
-}
+  body.addEventListener('keydown',function(e) {
+    if (e.which === codes[index]) {
+      index++
+       if (index===codes.length) {
+         alert("You did it!")
+         index = 0
+       }
+    } else {
+      index = 0
+    }
+  })}
